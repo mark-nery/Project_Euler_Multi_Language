6fdd5d7d459fd9a9c9d49156a4058635a2a7211f
mark nery <marknery@gmail.com>
2012-03-10 10:10:01 -0700

    added problem 3 C

---
+++ problem_3/problem-3.c
@@ -0,0 +1,33 @@
+//Project Euler #3
+//What is the largest prime factor of the number 600851475143 ?
+
+#include <stdio.h>
+
+int isPrime(int x){
+  if(x % 2 == 0){
+    return 0;
+  }
+  int i = 3;
+  while(i < x){
+    if(x % i == 0){
+      return 0;
+    }
+    i += 2;
+  }
+  return 1;
+}
+int main(int argc, char *argv[]){
+  
+  long long x = 600851475143LL;
+  int y = 2;
+
+  while(x > y){
+    if(x % y  == 0 && isPrime(y) == 1){
+      x = x / y;
+      y = 2;
+    }
+    y++;
+  }
+  printf("largest prime divisor = %lld\n",x);
+  return 0;
+}
